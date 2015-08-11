17cbbcefd3fcccd144907a6e558627356fd06649
Benjamin Linton <ben.linton@tablexi.com>
2015-08-10 22:42:05 -0500

    Deprecate unused osx aliases

--- modules/osx/aliases.bash.sh
+++ modules/osx/aliases.bash.sh
@@ -1,25 +1,2 @@
 #!/usr/bin/env bash
 
-# TODO: test for paths before setting aliases
-
-# ------------------------------------------------------------------------------
-# Sublime
-# https://www.sublimetext.com/docs/3/osx_command_line.html
-# ------------------------------------------------------------------------------
-alias subl="/Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl"
-#alias subl='/Applications/Sublime\ Text\ 2.app/Contents/SharedSupport/bin/subl'
-
-# ------------------------------------------------------------------------------
-# Textmate
-# ------------------------------------------------------------------------------
-#alias mate='/Applications/TextMate.app/Contents/SharedSupport/Support/bin/mate'
-
-# ------------------------------------------------------------------------------
-# MacVim
-# ------------------------------------------------------------------------------
-#alias mvim='/Applications/MacVim.app/Contents/...'
-
-# ------------------------------------------------------------------------------
-# Helper for opening Go2Shell app preferences
-# ------------------------------------------------------------------------------
-#alias open-go2shell-config='open -a Go2Shell --args config'
