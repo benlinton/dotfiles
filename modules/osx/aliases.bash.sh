1576091783bf402a0b6333c37249bd559cb2e22a
Ben Linton <benjamin.linton@gmail.com>
2015-12-19 17:12:25 -0600

    Add osx aliases for show and hide all files

--- modules/osx/aliases.bash.sh
+++ modules/osx/aliases.bash.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/env bash
 
+alias show-files="defaults write com.apple.finder AppleShowAllFiles YES; killall Finder /System/Library/CoreServices/Finder.app"
+alias hide-files="defaults write com.apple.finder AppleShowAllFiles NO; killall Finder /System/Library/CoreServices/Finder.app"
