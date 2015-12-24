0bea211d7761a1cb8891854b0aad38743d48769b
Ben Linton <ben.linton@tablexi.com>
2015-12-24 02:32:29 -0600

    Rename boostrap.sh to init.sh

--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #
-# This bootstrap file loads all bash functions and modules
+# This main bootstrap file loads all bash functions and modules
 
 # ------------------------------------------------------------------------------
 # Export global variables
@@ -8,21 +8,21 @@
 #export FOO=bar
 
 # ------------------------------------------------------------------------------
-# Source bash functions first
+# Source functions first
 # ------------------------------------------------------------------------------
 for file in $DOTFILES_HOME/functions/*; do source $file; done
 
 # ------------------------------------------------------------------------------
-# Source files that end with `.bash.first.sh`
+# Source files that end with `.autoload.first.sh`
 # ------------------------------------------------------------------------------
-find_and_source "*.bash.first.sh" $DOTFILES_HOME
+find_and_source "*.autoload.first.sh" $DOTFILES_HOME
 
 # ------------------------------------------------------------------------------
-# Source files that end with `.bash.sh`
+# Source files that end with `.autoload.sh`
 # ------------------------------------------------------------------------------
-find_and_source "*.bash.sh" $DOTFILES_HOME
+find_and_source "*.autoload.sh" $DOTFILES_HOME
 
 # ------------------------------------------------------------------------------
-# Source files that end with `.bash.last.sh`
+# Source files that end with `.autoload.last.sh`
 # ------------------------------------------------------------------------------
-find_and_source "*.bash.last.sh" $DOTFILES_HOME
+find_and_source "*.autoload.last.sh" $DOTFILES_HOME
