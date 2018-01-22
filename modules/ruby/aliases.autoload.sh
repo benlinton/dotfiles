aeb26df8efa91f63c2d6375b986dc1726fecd808
Ben Linton <ben.linton@tablexi.com>
2018-01-22 15:18:54 -0600

    Expand ruby aliases

--- modules/ruby/aliases.autoload.sh
+++ modules/ruby/aliases.autoload.sh
@@ -1,5 +1,15 @@
 #!/usr/bin/env bash
 
+# bundle
 alias be='bundle exec'
+
+# rbenv
 alias rbenv-binstubs='bundle install --binstubs .bundle/bin'
-alias enable-pry='unset DISABLE_PRY'
+
+# pry
+alias pry-enable='unset DISABLE_PRY'
+alias pry-disable='export DISABLE_PRY=true'
+
+# spring
+alias spring-enable='unset DISABLE_SPRING'
+alias spring-disable='export DISABLE_SPRING=true'
