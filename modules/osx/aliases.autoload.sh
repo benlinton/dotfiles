63acd2ad8e8842ad72c5e0d83be04aedf41432f1
Ben Linton <ben.linton@tablexi.com>
2018-01-22 15:17:45 -0600

    Append osx aliases

--- modules/osx/aliases.autoload.sh
+++ modules/osx/aliases.autoload.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
 
-alias show-files="defaults write com.apple.finder AppleShowAllFiles YES; killall Finder /System/Library/CoreServices/Finder.app"
-alias hide-files="defaults write com.apple.finder AppleShowAllFiles NO; killall Finder /System/Library/CoreServices/Finder.app"
+# Restart web camera without rebooting
+alias camera-restart="sudo killall VDCAssistant"
+
+# Show and hide files
+alias files-show="defaults write com.apple.finder AppleShowAllFiles YES; killall Finder /System/Library/CoreServices/Finder.app"
+alias files-hide="defaults write com.apple.finder AppleShowAllFiles NO; killall Finder /System/Library/CoreServices/Finder.app"
+
+# Reset audio; run with sudo
+alias audio-reset="kill -9 `ps ax | grep 'coreaudio[a-z]' | awk '{print $1}'`"
