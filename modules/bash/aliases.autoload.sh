ef1a5b9e8614fb7fc428720e90c8a91812137152
Ben Linton <ben.linton@tablexi.com>
2018-01-22 15:17:08 -0600

    Add generic functions-list alias

--- modules/bash/aliases.autoload.sh
+++ modules/bash/aliases.autoload.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
 # Output filesize of everything in pwd
-alias du_pwd='du -hd 1'
+alias du-pwd='du -hd 1'
 
 # Colorized, special characters (after dirs, executables, symlinks, etc.)
 alias ls='ls -GF'
+
+# List functions
+alias functions-list='typeset -F'
