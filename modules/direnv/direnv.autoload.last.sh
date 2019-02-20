543b3e44787afeafc936dc84793de48ac0c49069
Benjamin Linton <ben.linton@tablexi.com>
2019-02-20 09:57:00 -0600

    Add comments for direnv autoload

--- modules/direnv/direnv.autoload.last.sh
+++ modules/direnv/direnv.autoload.last.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# Read this for ideas on how to support aliases
+# https://github.com/direnv/direnv/issues/73
+
 if command_exists direnv ; then
   eval "$(direnv hook bash)"
 fi
